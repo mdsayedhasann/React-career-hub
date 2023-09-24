442035870eb087d4a862ff8a8a38e8a48113a875
sayed_hossain <sayed.hasan0011@gmail.com>
2023-09-24 21:06:47 +0600

    Featured Job Section complete dynamically and also added a button which will expand more data

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -3,6 +3,7 @@ import Job from '../Job/Job';
 
 const Featured = () => {
     const [jobs, setJobs] = useState([])
+    const [datalength, setDatalength] = useState(4)
     useEffect(() => {
         fetch('/public/jobs.json')
         .then(res => res.json())
@@ -10,13 +11,16 @@ const Featured = () => {
     } , [])
     return (
         <div className='text-center'>
-            <h1 className='text-5xl  my-8'>Featured Jobs {jobs.length} </h1>
+            <h1 className='text-5xl  my-8'>Featured Jobs</h1>
             <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
             
             <div className='grid grid-cols-2 text-left '>
             {
-                jobs.map(job => <Job key={job.id} job={job}></Job>)
+                jobs.slice(0, datalength).map(job => <Job key={job.id} job={job}></Job>)
             }
+            </div>
+            <div className={datalength === jobs.length && `hidden`}>
+                <button onClick={() => setDatalength(jobs.length)} className={`btn btn-primary bg-[#7E90FE] my-5 text-white `}>Show More</button>
             </div>
         </div>
     );
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,21 +1,62 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import React from "react";
+import { CiLocationOn } from "react-icons/Ci";
+import { AiOutlineDollar } from "react-icons/Ai";
+
 
 const Job = ({ job }) => {
-  const { id, logo, job_title, company_name, remote_or_onsite, location, job_type, salary, job_description, job_responsibility, educational_requirements,  experiences, contact_information     } = job;
+  const {
+    id,
+    logo,
+    job_title,
+    company_name,
+    remote_or_onsite,
+    location,
+    job_type,
+    salary,
+    job_description,
+    job_responsibility,
+    educational_requirements,
+    experiences,
+    contact_information,
+  } = job;
   return (
-    <div className="text-left my-5 py-3">
+    <div className="text-left my-5 py-3 ">
       <div className="card card-compact bordered border-red-950">
-        <figure>
-          <img className="w-40 h-16"
-            src={logo}
-            alt="Shoes"
-          />
-        </figure>
+       
         <div className="card-body">
-          <h2 className="card-title"> {job_title}</h2>
-          <p>{job_description}</p>
+
+        <div className="flex flex-row">
+        <figure className="">
+          <img className="w-24 h-10" src={logo} alt={company_name} title={company_name} />
+        </figure>
+        </div>
+          
+          <h2 className="card-title">{job_title}</h2>
+          <h3 className="text-xl">{company_name}</h3>
+          <div className="flex">
+            <button className="py-2 px-3 border border-[#9873FF] mr-2 text-[#9873FF] ">
+              {" "}
+              {remote_or_onsite}{" "}
+            </button>
+            <button className="py-2 px-3 border border-[#9873FF] mr-2 text-[#9873FF] ">
+              {" "}
+              {job_type}{" "}
+            </button>
+          </div>
+
+          <div className="flex justify-around">
+            <p className="text-xl py-2 px-3 text-[#757575] mr-2 flex items-center">
+                <CiLocationOn className="mr-3 -ml-4"></CiLocationOn> {location}
+            </p>
+            <p className="text-xl py-2 px-3 text-[#757575] flex items-center">
+             <AiOutlineDollar className="mr-3"></AiOutlineDollar> {salary}
+            </p>
+          </div>
+
           <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
+            <button className="btn btn-primary bg-[#9873FF] text-white">View Details</button>
           </div>
         </div>
       </div>
