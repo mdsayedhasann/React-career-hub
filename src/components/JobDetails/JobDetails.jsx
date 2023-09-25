6f9690c2795998819a9a6b8bdede8d6c3b5c6562
sayed_hossain <sayed.hasan0011@gmail.com>
2023-09-25 14:02:41 +0600

    Added Data to Local storage when clicked on Apply Now button

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,17 +1,38 @@
 import React from "react";
 import { useLoaderData, useParams } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { saveJobApplication } from "../../utility/localStorage";
 
 const JobDetails = () => {
   // eslint-disable-next-line no-unused-vars
   const jobs = useLoaderData();
-  const { id, job_title, job_description, job_responsibility } = useParams();
+  const { id } = useParams();
   const idInt = parseInt(id);
+
   const job = jobs.find((job) => job.id === idInt);
   console.log(id, job);
+
+  const notify = () =>
+  saveJobApplication(id);
+    toast.success("Job Added", {
+      position: "bottom-right",
+      autoClose: 1500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+
   return (
     <div className="max-w-6xl mx-auto py-4">
       <div className="bg-purple-50 w-full">
-        <h1 className="text-center text-3xl font-bold py-16">Job Details</h1>
+        <h1 className="text-center text-3xl font-bold py-16">
+          {" "}
+          {job.job_title}
+        </h1>
       </div>
       <div className="grid md:grid-cols-4">
         <div className="col-span-3">
@@ -40,7 +61,28 @@ const JobDetails = () => {
             </ul>
           </div>
         </div>
-        <div className="col-span-1"></div>
+        <div className="col-span-1 text-center mt-8">
+          <button
+            onClick={notify}
+            className="btn btn-primary text-center text-white"
+          >
+            Apply Now
+          </button>
+
+          <ToastContainer
+            position="bottom-right"
+            autoClose={2000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          />
+          {}
+        </div>
       </div>
     </div>
   );
---
+++ src/utility/localStorage.js
@@ -0,0 +1,22 @@
+
+const getStoredJobApplication = () => {
+    const storedJobApplication = localStorage.getItem('job-Application')
+    if(storedJobApplication){
+        return JSON.parse(storedJobApplication)
+        
+    }else
+    {
+        return[]
+    }
+}
+
+const saveJobApplication = id => { 
+    const storedJobApplication = getStoredJobApplication();
+    const exists = storedJobApplication.find(jobId => jobId === id)
+    if(!exists){
+        storedJobApplication.push(id); 
+        localStorage.setItem('job-Application', JSON.stringify(storedJobApplication))
+    }
+}
+
+export {saveJobApplication}
